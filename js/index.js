1af73674012487f5afac70ca9cb04bff850b4b15
zachary-vincent <45057145+ZachieV@users.noreply.github.com>
2019-04-12 19:48:35 -0700

    lil over half way there

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+const image = document.querySelector('.bus')
+
+image.addEventListener('click', function() {
+  const imageSource = event.target.src;
+  document.body.style.background = `url(${imageSource})`;
+})
+
+image.addEventListener('dblclick', function() {
+  imageSource = event.target.src;
+  document.body.style.background = 'white'
+})
+
+const anchor = document.querySelector('.nav')
+
+anchor.addEventListener('mouseover', function() {
+  document.querySelector('.nav').style.background = 'grey'
+})
+
+const type = document.querySelector('body')
+
+type.addEventListener('keydown', function(event) {
+  console.log(event)
+})
+
+
+
+const wheel = document.querySelector('.text-content')
+wheel.addEventListener("wheel", wheelFunction);
+
+function wheelFunction() {
+  this.style.width = '200px';
+}
+
+document.querySelector("a").onfocus = function() {
+  myFunction()
+};
+
+function myFunction() {
+  document.querySelector("a").style.color = "red";
+}
